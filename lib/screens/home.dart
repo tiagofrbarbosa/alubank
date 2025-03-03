c3192cbc435327b2b9600d4199be88b1700f0678
Tiago Freire Barbosa <tfreirebar@gmail.com>
2025-03-03 18:42:20 -0300

    income e spent section

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,9 @@
 import 'package:alubank/components/box_card.dart';
+import 'package:alubank/components/color_dot.dat.dart';
+import 'package:alubank/components/content_division.dart';
 import 'package:alubank/components/sections/header.dart';
+import 'package:alubank/components/sections/recent_activity.dart';
+import 'package:alubank/themes/theme_colors.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -11,7 +15,7 @@ class Home extends StatelessWidget {
       body: Column(
         children: [
           Header(),
-          BoxCard(boxContent: Text('Mia'))
+          RecentActivity(),
         ],
       ),
     );
